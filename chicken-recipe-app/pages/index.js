122a8b606c6e7085c4b4db87dc2e07c4b4079b59
rageebmahtab <rageeb.mahtab@rokt.com>
2024-01-08 18:19:34 -0500

    Fixed retrieving meals

--- chicken-recipe-app/pages/index.js
+++ chicken-recipe-app/pages/index.js
@@ -5,24 +5,37 @@ const inter = Inter({ subsets: ['latin'] })
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
 
 export default function Home() {
-  useEffect(async() => {
-    //console.log('Hello world');
-   await fetch(BASE_URL)
-    .then(response => response.json()).then(json => setMeals(json));
-  });
-
-  const [meals, setMeals] = useState([]);
+  const [meals, setMeals] = useState(null);
+  useEffect(() => {
+   // Define the function to fetch data
+   const fetchData = async () => {
+    try {
+      const response = await fetch(BASE_URL);
+      const json = await response.json();
+      const data = await json.meals;
+      setMeals(data);
+      
+      //setMeals(json.meals); // Make sure the structure of the response is correct
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  // Call the function
+  fetchData();
+  }, []);
 
   return (
     <>
-      <h1>Chicken recipes</h1> 
-      {meals.map((meal) => {
-        <ul>
-          <li>{meal.strMeal}</li>
-          <li>{meal.strMealThumb}</li>
-          <li>{meal.idMeal}</li>
-        </ul>
-      })}
+      <h1>Chicken recipes</h1>
+      <ul> 
+        {meals && meals.map((meal) => (
+          <li key={meal.idMeal}> 
+            <div>{meal.strMeal}</div>
+            <img src={meal.strMealThumb} alt={meal.strMeal} />
+            <div>{meal.idMeal}</div>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
